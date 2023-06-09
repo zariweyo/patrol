ea53d069039e8673f178f23abd1eb8efd77dc673
fylyppo <ftuzimek@gmail.com>
2023-06-09 16:24:27 +0200

    export

--- packages/patrol/example/ios/ci_scripts/ci_post_xcodebuild.sh
+++ packages/patrol/example/ios/ci_scripts/ci_post_xcodebuild.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 cd "$CI_WORKSPACE/packages/patrol/example"
 
+export PATH="$PATH:$HOME/flutter/bin"
+
 dart pub global activate \
     --source git https://github.com/leancodepl/patrol.git \
     --git-path packages/patrol_cli \
     --git-ref patrol_cli-v2.0.0-dev.2
     
 patrol build ios --target integration_test/example_test.dart
-
-export CI_TEST_PRODUCTS_PATH="$CI_WORKSPACE/packages/patrol/example/build/ios_integ/Build/Products"
