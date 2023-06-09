88b944bfd9e6413ac595fa2d1fa82f0189006c47
fylyppo <ftuzimek@gmail.com>
2023-06-09 16:51:15 +0200

    export .pub-cache/bin

--- packages/patrol/example/ios/ci_scripts/ci_post_xcodebuild.sh
+++ packages/patrol/example/ios/ci_scripts/ci_post_xcodebuild.sh
@@ -9,5 +9,7 @@ dart pub global activate \
     --source git https://github.com/leancodepl/patrol.git \
     --git-path packages/patrol_cli \
     --git-ref patrol_cli-v2.0.0-dev.2
+
+export PATH="$PATH":"$HOME/.pub-cache/bin"
     
 patrol build ios --target integration_test/example_test.dart
